080fd8f0b1695f05b7d2a7372f3b60fc19cab67a
mide7 <imharyomide@gmail.com>
2024-03-26 04:10:16 -0700

    update: claim daily reward modal UI done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,27 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class",
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#7C56FE",
-      },
-      backgroundImage: {
-        account: "url('/images/user-bg.png')",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: "class",
+	content: [
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			screens: {
+				xxxs: "320px",
+				xxs: "375px",
+				xs: "425px",
+			},
+			colors: {
+				primary: { DEFAULT: "#7C56FE", "50": "#F2EEFF" },
+			},
+			backgroundImage: {
+				account: "url('/images/user-bg.png')",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;

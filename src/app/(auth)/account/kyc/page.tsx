508c83e1770254cd9a6d0c62c4b71b916860fa02
arpeiks <arpeiks@gmail.com>
2024-04-03 09:45:11 +0100

    fix: build error

--- src/app/(auth)/account/kyc/page.tsx
+++ src/app/(auth)/account/kyc/page.tsx
@@ -1,31 +1,5 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import React from "react";
-import Persona from "persona";
-import PageLoader from "@/components/loader";
+const Page = dynamic(() => import("@/containers/account/kyc"), { ssr: false });
 
-const InlineInquiry = () => {
-  const [ready, setReady] = React.useState(false);
-  const templateId = process.env.NEXT_PUBLIC_PERSONA_TEMPLATE_ID;
-  const environmentId = process.env.NEXT_PUBLIC_PERSONA_ENVIRONMENT_ID;
-
-  return (
-    <div className="h-screen w-full [&_iframe]:h-screen [&_iframe]:w-full">
-      {!ready && <PageLoader />}
-
-      <Persona.Inquiry
-        templateId={templateId}
-        environmentId={environmentId}
-        onReady={() => setReady(true)}
-        onLoad={() => console.log("Loaded inline")}
-        onComplete={({ inquiryId, status, fields }) => {
-          // Inquiry completed. Optionally tell your server about it.
-          console.log("status", status, "fields", fields);
-          console.log(`Sending finished inquiry ${inquiryId} to backend`);
-        }}
-      />
-    </div>
-  );
-};
-
-export default InlineInquiry;
+export default Page;

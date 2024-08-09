7a20d7501b4003a5b8f8c92497bd17d4ee48f1d0
dorteks <ayodelealayemi@gmail.com>
2024-08-09 09:08:37 +0100

    fix: updated floating component

--- src/containers/layout/auth/auth-nav.layout.tsx
+++ src/containers/layout/auth/auth-nav.layout.tsx
@@ -11,7 +11,7 @@ const AuthNavLayout = ({ children }: { children: React.ReactNode }) => {
   const shouldHideFooterNav = pathname.includes("/bot");
 
   return (
-    <div className="flex flex-col h-screen">
+    <div className="relative flex flex-col h-screen mx-auto">
       <div className="flex-1 text-white">{children}</div>
       <FloatingIcon />
       {!shouldHideFooterNav && <FooterNav />}

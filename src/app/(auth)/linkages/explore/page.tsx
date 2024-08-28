70d69e1198805405d521907544c468f8dac09a1d
dorteks <ayodelealayemi@gmail.com>
2024-08-28 10:09:48 +0100

    updated linkage page UI

--- src/app/(auth)/linkages/explore/page.tsx
+++ src/app/(auth)/linkages/explore/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -9,8 +9,20 @@ import FeaturedLinkages from "@/containers/linkages/featured-linkages-card";
 import { linkagesArray } from "@/lib/values/mockData";
 import { useGetDiscoveryLinkages, useGetLinkages } from "@/api/ai";
 
+const tabs = [
+  { name: "For you", value: "for-you" },
+  { name: "Following", value: "following" },
+  { name: "My Linkages", value: "my-linkages" },
+  { name: "DMs (Direct Messages)", value: "dms" },
+];
+
 const Linkages = () => {
   const router = useRouter();
+
+  const [selectedTab, setSelectedTab] = useState("for-you");
+  const handleTabClick = (tab: string) => {
+    setSelectedTab(tab);
+  };
 
   const {
     data: discoveryLinkages,
@@ -40,65 +52,120 @@ const Linkages = () => {
           <SearchComponent />
         </div>
 
-        {linkages?.data && (
-          <div className="flex flex-col gap-3">
-            <div>
-              <p className="text-lg font-semibold">My Linkages</p>
-              <p className="text-sm">Linkages you created</p>
-            </div>
+        <div className="px-1 flex flex-row gap-2 w-[inherit] border-b border-[#F2EEFF40] overflow-x-auto">
+          {tabs.map((tab) => (
+            <button
+              key={tab.value}
+              onClick={() => handleTabClick(tab.value)}
+              className={`min-w-fit px-3 py-3 ${
+                selectedTab === tab.value
+                  ? "text-white border-b-2 border-b-white"
+                  : "bg-transparent text-[#F2EEFF]"
+              }`}
+            >
+              {tab.name}
+            </button>
+          ))}
+        </div>
 
-            {linkages?.data.map((i: any) => (
-              <FeaturedLinkages
-                key={i.name}
-                name={i.name}
-                image={i.image}
-                description={i.description}
-                author={i.userId}
-                slug={i.slug}
-              />
-            ))}
+        <div className="flex">
+          {/* // For you */}
+          <div>
+            {selectedTab === "for-you" && (
+              <div className="flex flex-col gap-10">
+                {discoveryLinkages?.data?.data && (
+                  <div className="flex flex-col gap-3">
+                    <div>
+                      <p className="text-lg font-semibold">Featured Linkages</p>
+                      <p className="text-sm">
+                        Top Picks from this week activities
+                      </p>
+                    </div>
+
+                    {discoveryLinkages?.data?.data.map((i: any) => (
+                      <FeaturedLinkages
+                        key={i.name}
+                        name={i.name}
+                        image={i.image}
+                        description={i.description}
+                        author={i.userId}
+                        slug={i.slug}
+                      />
+                    ))}
+                  </div>
+                )}
+                {discoveryLinkages?.data?.data && (
+                  <div className="flex flex-col gap-3">
+                    <div>
+                      <p className="text-lg font-semibold">
+                        Recommended for you
+                      </p>
+                      <p className="text-sm">Linkages based on your response</p>
+                    </div>
+
+                    {discoveryLinkages?.data?.data.map((i: any) => (
+                      <FeaturedLinkages
+                        key={i.name}
+                        name={i.name}
+                        image={i.image}
+                        description={i.description}
+                        author={i.userId}
+                        slug={i.slug}
+                      />
+                    ))}
+                  </div>
+                )}
+              </div>
+            )}
           </div>
-        )}
 
-        {discoveryLinkages?.data?.data && (
-          <div className="flex flex-col gap-3">
-            <div>
-              <p className="text-lg font-semibold">Featured Linkages</p>
-              <p className="text-sm">Top Picks from this week activities</p>
-            </div>
-
-            {discoveryLinkages?.data?.data.map((i: any) => (
-              <FeaturedLinkages
-                key={i.name}
-                name={i.name}
-                image={i.image}
-                description={i.description}
-                author={i.userId}
-                slug={i.slug}
-              />
-            ))}
+          {/* Following */}
+          <div>
+            {selectedTab === "following" && (
+              <div className="bg-red-500">
+                <p>Linkages you follow will appear here</p>
+              </div>
+            )}
           </div>
-        )}
 
-        {discoveryLinkages?.data?.data && (
-          <div className="flex flex-col gap-3">
-            <div>
-              <p className="text-lg font-semibold">Recommended for you</p>
-              <p className="text-sm">Linkages based on your response</p>
-            </div>
+          {/* my linkages */}
+          <div>
+            {selectedTab === "my-linkages" && (
+              <div>
+                {linkages?.data && (
+                  <div className="flex flex-col gap-3">
+                    <div>
+                      <p className="text-lg font-semibold">My Linkages</p>
+                      <p className="text-sm">Linkages you created</p>
+                    </div>
 
-            {discoveryLinkages?.data?.data.map((i: any) => (
-              <FeaturedLinkages
-                key={i.name}
-                name={i.name}
-                image={i.image}
-                description={i.description}
-                author={i.userId}
-                slug={i.slug}
-              />
-            ))}
+                    {linkages?.data.map((i: any) => (
+                      <FeaturedLinkages
+                        key={i.name}
+                        name={i.name}
+                        image={i.image}
+                        description={i.description}
+                        author={i.userId}
+                        slug={i.slug}
+                      />
+                    ))}
+                  </div>
+                )}
+              </div>
+            )}
           </div>
-        )}
+
+          {/* DMs */}
+          <div>
+            {selectedTab === "dms" && (
+              <div>
+                <p>
+                  DMs and interactions you have with linkages will appear here
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </main>
   );

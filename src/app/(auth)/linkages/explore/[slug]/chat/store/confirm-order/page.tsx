4da752b403d3ff503f53a4bf16f16c1e50f6fc48
dorteks <ayodelealayemi@gmail.com>
2024-10-01 06:05:22 +0100

    updated item order UI flow

--- src/app/(auth)/linkages/explore/[slug]/chat/store/confirm-order/page.tsx
+++ src/app/(auth)/linkages/explore/[slug]/chat/store/confirm-order/page.tsx
@@ -1,22 +1,29 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Button from "@/components/button";
-import { Add, ArrowLeft2, InfoCircle, Minus } from "iconsax-react";
+import { Add, ArrowLeft2, Icon, InfoCircle, Minus } from "iconsax-react";
 import { useParams, useRouter } from "next/navigation";
 import { SpinnerIcon } from "@/components/icons/spinner";
 import { useCart } from "@/provider/cart-context-provider";
 import { Plus } from "lucide-react";
+import PaymentOrderSuccessful from "@/containers/linkages/payment-successful-modal";
 
 const ConfirmOrder = () => {
   const router = useRouter();
   const params = useParams();
   const slug = params.slug;
 
-  const isPending = false;
-
   const { cartItems } = useCart();
+
+  const [openSuccessModal, setOpenSuccessModal] = useState(false);
+
+  const handlePayment = () => {
+    setOpenSuccessModal(true);
+  };
+
+  const isPending = false;
 
   // Calculate subtotal
   const subtotal = cartItems.reduce((acc, cartItem) => {
@@ -39,41 +46,58 @@ const ConfirmOrder = () => {
         <p className="text-lg font-semibold">Confirm Order</p>
       </div>
 
-      <section className="flex-1 flex flex-col gap-6 mt-6 w-full overflow-auto">
-        <p className="mb-2">Order Summary</p>
+      <section className="flex-1 flex flex-col gap-4 mt-6 w-full overflow-auto">
+        <p className="text-base font-semibold mb-2">Order Summary</p>
 
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2 text-sm">
           <div className="flex flex-row items-center justify-between">
-            <p>{cartItems.length} items</p>
-            <p>{subtotal.toFixed(2)} usdc</p>
+            <p className="font-normal text-[#F6F1FE]">
+              {cartItems.length} items
+            </p>
+            <p className="font-semibold">{subtotal.toFixed(2)} usdc</p>
           </div>
 
           <div className="flex flex-row items-center justify-between">
-            <p>Delivery fees</p>
-            <p>{deliveryFee.toFixed(2)} usdc</p>
+            <p className="font-normal text-[#F6F1FE]">Delivery fees</p>
+            <p className="font-semibold">{deliveryFee.toFixed(2)} usdc</p>
           </div>
 
           <div className="flex flex-row items-center justify-between">
-            <p>Total</p>
-            <p>{totalFee.toFixed(2)} usdc</p>
+            <p className="font-normal text-[#F6F1FE]">Total</p>
+            <p className="font-semibold text-base">
+              {totalFee.toFixed(2)} usdc
+            </p>
           </div>
         </div>
 
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-row items-center justify-between">
-            <p>Delivery address</p>
+        <div className="flex flex-col gap-2 mt-4">
+          <div className="flex flex-row items-center justify-between text-[#DFCBFB]">
+            <p className="text-base font-semibold">Delivery address</p>
             <button
               onClick={() => {}}
               className="flex flex-row flex-end items-center justify-end gap-2 py-2 mt-2 text-sm text-start text-[#DFCBFB] font-medium"
             >
               <div className="p-[3px] rounded-full border border-[#FFFFFF36]">
                 <Plus size={14} />
-              </div>{" "}
+              </div>
               Add
             </button>
           </div>
-          <div className="border border-[#FFFFFF36] rounded-[12px] p-4">
-            Toluwase Adebayo
+
+          <div className="flex flex-row items-start gap-1.5 border border-[#FFFFFF36] rounded-[12px] p-4">
+            <div>
+              <Icon />
+            </div>
+            <div className="flex flex-col gap-1">
+              <p className="text-[15px] font-medium">Toluwase Adebayo</p>
+              <p className="text-[13px] font-normal text-[#E5E7EB]">
+                No 25, Ribbon Street, three way junction, Ikeja, Lagos
+              </p>
+            </div>
+
+            <div className="flex items-end self-end ml-4 text-sm font-medium">
+              <button>Edit</button>
+            </div>
           </div>
         </div>
       </section>
@@ -83,13 +107,17 @@ const ConfirmOrder = () => {
           <InfoCircle size={16} />
           <p> {totalFee.toFixed(2)} will be charged from your USDC wallet</p>
         </div>
-        <Button
-          onClick={() => console.log("exectuer payment")}
-          disabled={isPending}
-        >
+        <Button onClick={handlePayment} disabled={isPending}>
           {isPending ? <SpinnerIcon /> : "Confirm and Pay"}
         </Button>
       </div>
+
+      {openSuccessModal && (
+        <PaymentOrderSuccessful
+          isOpen={openSuccessModal}
+          onClose={() => setOpenSuccessModal(false)}
+        />
+      )}
     </main>
   );
 };

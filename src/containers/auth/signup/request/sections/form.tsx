3897ff2dba565811bc7715d6100c80b07b292179
dorteks <ayodelealayemi@gmail.com>
2024-10-18 08:46:33 +0100

    added device location detection implementation

--- src/containers/auth/signup/request/sections/form.tsx
+++ src/containers/auth/signup/request/sections/form.tsx
@@ -1,10 +1,10 @@
 "use client";
 
+import { useEffect } from "react";
 import { useAtom } from "jotai";
 import { authAtom } from "@/lib/atoms/auth.atom";
 import PhoneInput from "@/components/input/phone-input";
 import CountrySelect from "@/components/select/country-select";
-import { PhoneNumber } from "libphonenumber-js";
 
 const FormInput = () => {
   const [state, setState] = useAtom(authAtom);
@@ -17,13 +17,40 @@ const FormInput = () => {
     setState((prev) => ({ ...prev, phoneNumber }));
   };
 
-  const country = state.country ? JSON.parse(state.country)?.code : null;
+  const countryData = state.country ? JSON.parse(state.country) : null;
+  const countryCode = countryData?.code || null;
+
+  const successCallback = async (position: GeolocationPosition) => {
+    const { latitude, longitude } = position.coords;
+
+    const response = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      const countryCode = data.countryCode;
+      const countryName = data.countryName;
+
+      if (countryCode) {
+        setCountry(JSON.stringify({ code: countryCode, name: countryName }));
+      }
+    }
+  };
+
+  const errorCallback = (error: GeolocationPositionError) => {
+    console.log(error);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  }, []);
 
   return (
     <>
       <CountrySelect value={state.country} setValue={setCountry} />
       <PhoneInput
-        country={country}
+        country={countryCode}
         value={state.phoneNumber}
         setValue={setPhoneNumber}
       />
